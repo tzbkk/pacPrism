59ee56619fe6fa4976ab67269ec72cc1177b5133
Haolin Xue <tzbkk@outlook.com>
2025-11-23 17:04:10 +0800

    update dht_opration

--- include/node/dht/dht_operation.h
+++ include/node/dht/dht_operation.h
@@ -1,18 +1,26 @@
 // This head exports interface to call DHT_operation related functions:
 // dht_operation::store_entry()
+// dht_operation::query_entry()
+// dht_operation::remove_entry()
 // dht_operation::stored_entries
+// dht_operation::map_node_ip_to_stored_entries_index
 
 #ifndef DHT_OPERATION_H
 #define DHT_OPERATION_H
 
 #include "node/dht/dht_types.h"
 #include <vector>
+#include <map>
 
 class dht_operation {
     private:
         std::vector<dht_entry> stored_entries;
+        std::map<std::string, int> map_node_ip_to_stored_entries_index;
     public:
         void store_entry(dht_entry entry);
+        dht_entry query_entry(const std::string& node_ip) const;
+        std::vector<dht_entry> query_entry(const sharding& sharding_querying) const;
+        void remove_entry(std::string node_ip);
 };
 
 #endif
--- lib/node/dht/dht_operation.cpp
+++ lib/node/dht/dht_operation.cpp
@@ -1,6 +1,43 @@
 #include "node/dht/dht_operation.h"
 #include <vector>
 
+// Store entry to the vector stored_entries.
 void dht_operation::store_entry(dht_entry entry) {
-    dht_operation::stored_entries.push_back(std::move(entry));
+    // Whether the entry exist. If not, store a new entry.
+    if (map_node_ip_to_stored_entries_index.find(entry.node_ip) == map_node_ip_to_stored_entries_index.end()) {
+        // Update map.
+        map_node_ip_to_stored_entries_index[entry.node_ip] = stored_entries.size() - 1;
+        // Push the new entry.
+        dht_operation::stored_entries.push_back(std::move(entry));
+    }
+    // If exist, check the timestamp. Store the newer one.
+    else {
+        // Get the index of the entry.
+        int stored_entry_index = map_node_ip_to_stored_entries_index[entry.node_ip];
+        // Check the timestamp and update.
+        if (stored_entries[stored_entry_index].entry_timestamp < entry.entry_timestamp) {
+            stored_entries[stored_entry_index].node_sharding = entry.node_sharding;
+            stored_entries[stored_entry_index].entry_timestamp = entry.entry_timestamp;
+            stored_entries[stored_entry_index].node_ttl = entry.node_ttl;
+        }
+    }
+}
+
+// Query an entry by node_ip.
+dht_entry dht_operation::query_entry(const std::string& node_ip) const {
+    auto query_result = map_node_ip_to_stored_entries_index.find(node_ip);
+    if (query_result != map_node_ip_to_stored_entries_index.end()) {
+        return stored_entries[query_result->second];
+    }
+    return dht_entry{};
+}
+
+// Query entries by sharding.
+std::vector<dht_entry> dht_operation::query_entry(const sharding& sharding_querying) const {
+
+}
+
+// Remove entry from the vector stored_entries.
+void dht_operation::remove_entry(std::string node_ip) {
+    
 }
