08f0414cafafd6d7f8c117e7bcdf0f139204cd6c
Haolin Xue <tzbkk@outlook.com>
2025-11-23 15:38:04 +0800

    renamed dht_store to dht_operation and completed dht_operation::store_entry().

--- include/node/dht/dht_operation.h
+++ include/node/dht/dht_operation.h
@@ -1,4 +1,18 @@
+// This head exports interface to call DHT_operation related functions:
+// dht_operation::store_entry()
+// dht_operation::stored_entries
+
 #ifndef DHT_OPERATION_H
 #define DHT_OPERATION_H
+
+#include "node/dht/dht_types.h"
+#include <vector>
+
+class dht_operation {
+    private:
+        std::vector<dht_entry> stored_entries;
+    public:
+        void store_entry(dht_entry entry);
+};
 
 #endif
--- lib/node/dht/dht_operation.cpp
+++ lib/node/dht/dht_operation.cpp
@@ -1,1 +1,6 @@
 #include "node/dht/dht_operation.h"
+#include <vector>
+
+void dht_operation::store_entry(dht_entry entry) {
+    dht_operation::stored_entries.push_back(std::move(entry));
+}
