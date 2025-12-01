5e78e2efca8baa3a9d6769be4575915387378770
Haolin Xue <tzbkk@outlook.com>
2025-12-01 20:10:29 +0800

    Created a pure gh-pages branch.

--- scripts/BuildConfig.cmake
+++
@@ -1,4 +0,0 @@
-# BuildConfig.cmake - Build configuration for pacPrism
-
-# Build configuration
-set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/bin)
--- scripts/PlatformConfig.cmake
+++
@@ -1,6 +0,0 @@
-# PlatformConfig.cmake - Platform-specific configurations for pacPrism
-
-# Platform-specific definitions
-if(WIN32)
-    add_compile_definitions(_WIN32_WINNT=0x0601)
-endif()
